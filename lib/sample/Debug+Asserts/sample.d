29d209762c7fe242538063b53753ccbacff15191
lirenlin <lirenlin@gmail.com>
2012-06-21 18:27:43 +0200

    local

--- lib/sample/Debug+Asserts/sample.d
+++ lib/sample/Debug+Asserts/sample.d
@@ -1,33 +1,20 @@
 /home/lirenlin/Documents/hiwi/projects/MemroyDependenceAnalysis/lib/sample/Debug+Asserts/sample.o \
  /home/lirenlin/Documents/hiwi/projects/MemroyDependenceAnalysis/lib/sample/Debug+Asserts/sample.d: \
- sample.cpp /home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassSupport.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassRegistry.h \
+ sample.cpp \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/MemoryDependenceAnalysis.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/BasicBlock.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Instruction.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/User.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ErrorHandling.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Compiler.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/StringRef.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/type_traits.h \
  /home/lirenlin/Documents/hiwi/build/include/llvm/Support/DataTypes.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/InitializePasses.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Atomic.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Valgrind.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Compiler.h \
- /home/lirenlin/Documents/hiwi/build/include/llvm/Config/llvm-config.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassAnalysisSupport.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallVector.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassManager.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Function.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/GlobalValue.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Constant.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/User.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ErrorHandling.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Value.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Use.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/PointerIntPair.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Casting.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/CallingConv.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/BasicBlock.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Instruction.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/ilist_node.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/DebugLoc.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/DenseMapInfo.h \
@@ -35,12 +22,24 @@
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/SymbolTableListTraits.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/ilist.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/Twine.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Argument.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassSupport.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassRegistry.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/InitializePasses.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Atomic.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Valgrind.h \
+ /home/lirenlin/Documents/hiwi/build/include/llvm/Config/llvm-config.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/PassAnalysisSupport.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallVector.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ValueHandle.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/AliasAnalysis.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/CallSite.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Attributes.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/MathExtras.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/SwapByteOrder.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/ArrayRef.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/raw_ostream.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/CallingConv.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Instructions.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/InstrTypes.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/OperandTraits.h \
@@ -48,58 +47,41 @@
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Type.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/IntegersSubset.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Constants.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Constant.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/APInt.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/APFloat.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/LLVMContext.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/IntegersSubsetMapping.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/InstIterator.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/MemoryDependenceAnalysis.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ValueHandle.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/AliasAnalysis.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/CallSite.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/DenseMap.h \
  /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallPtrSet.h \
- /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/OwningPtr.h
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/OwningPtr.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/Passes.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Assembly/Writer.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/InstIterator.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Function.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/GlobalValue.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Argument.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/raw_ostream.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SetVector.h \
+ /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallSet.h
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/MemoryDependenceAnalysis.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassSupport.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/BasicBlock.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Instruction.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassRegistry.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/User.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Compiler.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/StringRef.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/type_traits.h:
 
 /home/lirenlin/Documents/hiwi/build/include/llvm/Support/DataTypes.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/InitializePasses.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Atomic.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Valgrind.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Compiler.h:
-
-/home/lirenlin/Documents/hiwi/build/include/llvm/Config/llvm-config.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassAnalysisSupport.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallVector.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassManager.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Function.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/GlobalValue.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Constant.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/User.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ErrorHandling.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Value.h:
 
@@ -110,12 +92,6 @@
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Casting.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/CallingConv.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/BasicBlock.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Instruction.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/ilist_node.h:
 
@@ -131,7 +107,31 @@
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/Twine.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Argument.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassSupport.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Pass.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassRegistry.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/InitializePasses.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Atomic.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/Valgrind.h:
+
+/home/lirenlin/Documents/hiwi/build/include/llvm/Config/llvm-config.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ValueHandle.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/AliasAnalysis.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/CallSite.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Attributes.h:
 
@@ -141,7 +141,7 @@
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/ArrayRef.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/raw_ostream.h:
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/CallingConv.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Instructions.h:
 
@@ -157,6 +157,8 @@
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Constants.h:
 
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Constant.h:
+
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/APInt.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/APFloat.h:
@@ -165,18 +167,26 @@
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/IntegersSubsetMapping.h:
 
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/InstIterator.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/MemoryDependenceAnalysis.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/ValueHandle.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/AliasAnalysis.h:
-
-/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/CallSite.h:
-
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/DenseMap.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallPtrSet.h:
 
 /home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/OwningPtr.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Analysis/Passes.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Assembly/Writer.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/InstIterator.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Function.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/GlobalValue.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Argument.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/Support/raw_ostream.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SetVector.h:
+
+/home/lirenlin/Documents/hiwi/llvm/include/llvm/ADT/SmallSet.h:
--- lib/sample/sample.cpp
+++ lib/sample/sample.cpp
@@ -1,12 +1,12 @@
-#include <vector>
-
-#include "llvm/Pass.h"
-#include "llvm/PassManager.h"
-#include "llvm/Function.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/Instructions.h"
-#include "llvm/Support/InstIterator.h"
 #include "llvm/Analysis/MemoryDependenceAnalysis.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/Assembly/Writer.h"
+#include "llvm/Support/CallSite.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/SetVector.h"
 
 
 using namespace llvm;
@@ -20,58 +20,164 @@ namespace {
         public:
             variablePass(): FunctionPass(ID){}
             static char ID;
+            Function *F;
+
+            enum DepType {
+                Clobber = 0,
+                Def,
+                NonFuncLocal,
+                Unknown
+            };
+            typedef PointerIntPair<const Instruction *, 2, DepType> InstTypePair;
+            typedef std::pair<InstTypePair, const BasicBlock *> Dep;
+            typedef SmallSetVector<Dep, 4> DepSet;
+            typedef DenseMap<const Instruction *, DepSet> DepSetMap;
 
             virtual bool runOnFunction(Function &F);
             virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+            void print(raw_ostream &OS, const Module *M) const; 
+            static InstTypePair getInstTypePair(MemDepResult dep) {
+                if (dep.isClobber())
+                    return InstTypePair(dep.getInst(), Clobber);
+                if (dep.isDef())
+                    return InstTypePair(dep.getInst(), Def);
+                if (dep.isNonFuncLocal())
+                    return InstTypePair(dep.getInst(), NonFuncLocal);
+                assert(dep.isUnknown() && "unexptected dependence type");
+                return InstTypePair(dep.getInst(), Unknown);
+            }
+
+            static InstTypePair getInstTypePair(const Instruction* inst, DepType type) {
+                return InstTypePair(inst, type);
+            }
+        private:
+
+            static const char *const DepTypeStr[];
+            DepSetMap Deps;
 
 
     };
 }
 
 char variablePass::ID = 0;
+const char *const variablePass::DepTypeStr[]
+= {"Clobber", "Def", "NonFuncLocal", "Unknown"};
+
 
 bool variablePass::runOnFunction(Function &F)
 {
+    this->F = &F;
+
     errs() << "In function: ";
     errs().write_escaped(F.getName()) << '\n';
 
-    /// used to store memory related instructin(include potential ones, e.g. call)
-    std::vector<Instruction *> instList;
+    MemoryDependenceAnalysis &MDA = getAnalysis<MemoryDependenceAnalysis>();
+    AliasAnalysis &AA = getAnalysis<AliasAnalysis>();
 
     for(inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i)
-        switch (i->getOpcode())
-        {
+    {
+        Instruction *inst = &*i;
+        /// if the instruction may access the memory
+        if(!inst->mayReadFromMemory() && !inst->mayWriteToMemory())
+            continue;
 
-            case Instruction::Alloca:
-                instList.push_back(&*i);
-                break;
-            case Instruction::Load:
-                instList.push_back(&*i);
-                break;
-            case Instruction::Store:
-                instList.push_back(&*i);
-                break;
-            case Instruction::Call:
-                instList.push_back(&*i);
-                break;
-            default: break;
+        MemDepResult Res = MDA.getDependency(inst);
+        /// local
+        if (!Res.isNonLocal()) {
+            /// add the result into the dependency vector
+            Deps[inst].insert(std::make_pair(getInstTypePair(Res),
+                        static_cast<BasicBlock *>(0)));
+            /// function call
+        } else if (CallSite CS = cast<Value>(inst)) {
+            const MemoryDependenceAnalysis::NonLocalDepInfo &NLDI =
+                MDA.getNonLocalCallDependency(CS);
+
+            DepSet &InstDeps = Deps[inst];
+            for (MemoryDependenceAnalysis::NonLocalDepInfo::const_iterator
+                    I = NLDI.begin(), E = NLDI.end(); I != E; ++I) {
+                const MemDepResult &Res = I->getResult();
+                InstDeps.insert(std::make_pair(getInstTypePair(Res), I->getBB()));
+            }
+        } else {
+            SmallVector<NonLocalDepResult, 4> NLDI;
+            if (LoadInst *LI = dyn_cast<LoadInst>(inst)) {
+                if (!LI->isUnordered()) {
+                    // FIXME: Handle atomic/volatile loads.
+                    Deps[inst].insert(std::make_pair(getInstTypePair(0, Unknown),
+                                static_cast<BasicBlock *>(0)));
+                    continue;
+                }
+                AliasAnalysis::Location Loc = AA.getLocation(LI);
+                MDA.getNonLocalPointerDependency(Loc, true, LI->getParent(), NLDI);
+            } else if (StoreInst *SI = dyn_cast<StoreInst>(inst)) {
+                if (!SI->isUnordered()) {
+                    // FIXME: Handle atomic/volatile stores.
+                    Deps[inst].insert(std::make_pair(getInstTypePair(0, Unknown),
+                                static_cast<BasicBlock *>(0)));
+                    continue;
+                }
+                AliasAnalysis::Location Loc = AA.getLocation(SI);
+                MDA.getNonLocalPointerDependency(Loc, false, SI->getParent(), NLDI);
+            } else if (VAArgInst *VI = dyn_cast<VAArgInst>(inst)) {
+                AliasAnalysis::Location Loc = AA.getLocation(VI);
+                MDA.getNonLocalPointerDependency(Loc, false, VI->getParent(), NLDI);
+            } else {
+                llvm_unreachable("Unknown memory instruction!");
+            }
+
+            DepSet &InstDeps = Deps[inst];
+            for (SmallVectorImpl<NonLocalDepResult>::const_iterator
+                    I = NLDI.begin(), E = NLDI.end(); I != E; ++I) {
+                const MemDepResult &Res = I->getResult();
+                InstDeps.insert(std::make_pair(getInstTypePair(Res), I->getBB()));
+            }
         }
-
-    errs() << "total number of memory instruction " <<  instList.size() <<'\n';
-
-    
-    MemoryDependenceAnalysis &mda = getAnalysis<MemoryDependenceAnalysis>();
-    for(int i = 0, size = instList.size(); i < size; i++)
-        MemDepResult result = mda.getDependency(instList[i]);
-
+    }
+    print(errs(), NULL);
     return false;
 }
 
 void variablePass::getAnalysisUsage(AnalysisUsage &AU) const 
 {
-      //AU.addRequiredTransitive<AliasAnalysis>();
-      AU.addRequiredTransitive<MemoryDependenceAnalysis>();
-      AU.setPreservesAll();
+    AU.addRequiredTransitive<AliasAnalysis>();
+    AU.addRequiredTransitive<MemoryDependenceAnalysis>();
+    AU.setPreservesAll();
+}
+
+void variablePass::print(raw_ostream &OS, const Module *M) const {
+    for (inst_iterator I = inst_begin(*F), E = inst_end(*F); I != E; ++I) {
+        const Instruction *Inst = &*I;
+
+        DepSetMap::const_iterator DI = Deps.find(Inst);
+        if (DI == Deps.end())
+            continue;
+
+        const DepSet &InstDeps = DI->second;
+
+        for (DepSet::const_iterator I = InstDeps.begin(), E = InstDeps.end();
+                I != E; ++I) {
+            const Instruction *DepInst = I->first.getPointer();
+            DepType type = I->first.getInt();
+            const BasicBlock *DepBB = I->second;
+
+            /*
+            OS << "    ";
+            OS << DepTypeStr[type];
+            if (DepBB) {
+                OS << " in block ";
+                WriteAsOperand(OS, DepBB, false, M);
+            }
+            if (DepInst) {
+                OS << " from: ";
+                DepInst->print(OS);
+            }
+            OS << "\n";
+            */
+        }
+        OS << "****";
+        Inst->print(OS);
+        OS << "****" << '\n';
+    }
 }
 
 static RegisterPass<variablePass> X("variablePass", "variable scan Pass", false, true);
